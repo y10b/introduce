2dfab0cee46743981379473c0f6e2b2b9cd89ce3
y10b <victory.jun01@gmail.com>
2025-02-25 17:36:38 +0900

    img

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const mbtiType = 'ESTJ';
   const name = '김승준';
   const imageUrl = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDExMTRfMTU1%2FMDAxNzMxNTQyMDY5NDgz.Xp7nxhEQV3_zEl9vO78riF_WIiBSvj5QY4CQtOjdWgQg.JIuWiagytIpOmySq7XeJv0SJ6bnmAfDFAwclkn5jYZsg.JPEG%2FIMG_3748.JPG&type=sc960_832';  // 이미지 URL (예시)
-  const imageDescription = '네잎클로버 보고 가세요~' ;
+  const imageDescription = '🍀네잎클로버 보고 가세요~' ;
 
   return (
     <div className="App">
